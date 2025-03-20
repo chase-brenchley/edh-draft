38252f3c386d002e23171223011826ff2f29bb8f
Chase Brenchley <chase.brenchley@modea.com>
2025-03-20 16:16:59 -0600

    more space between export

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.5 }}
+              className="space-y-12"
             >
               <CommanderSelection />
               <DraftInterface />
